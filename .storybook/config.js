31c01bf6bcbdd083058659989389154705cf03e0
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-09 20:55:14 +0900

    change storybook dir

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,4 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /\.stories\.tsx$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context("../stories", true, /\.stories\.tsx$/), module);
