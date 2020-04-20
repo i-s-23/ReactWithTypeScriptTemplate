457bc0dc38328abe4bdad9faccc43ca76d03fde0
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-20 18:52:55 +0900

    下記を参考にしてBabelからreact-docgen-typescript-loaderに修正。soriesファイルの場所も変更。 'https://qiita.com/otanu/items/dd06d6702f673268c74e'

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,21 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { setConsoleOptions } from '@storybook/addon-console';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
 
-// automatically import all files ending in *.stories.js
-configure(require.context("../stories", true, /\.stories\.tsx$/), module);
+setConsoleOptions({
+    panelExclude: []
+  });
+
+addDecorator(withInfo);
+addDecorator(withKnobs);
+addDecorator(withA11y);
+
+const req = require.context("../src", true, /\.stories\.tsx$/);
+
+function loadStories() {
+    req.keys().forEach(filename => req(filename));
+  }
+
+  configure(loadStories, module);
